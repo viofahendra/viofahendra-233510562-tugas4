d48c096e20edce8bbccec831bc9549b50c5dd689
qadrihafyan <viofahendra@student.uir.ac.id>
2025-06-25 22:14:42 +0700

    port update

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ const db = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || 'mysql', // ambil dari .env
     port: process.env.DB_PORT,
   });
 
