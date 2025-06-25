837e9a5d6eedf609146212008dde49796b642ce9
qadrihafyan <viofahendra@student.uir.ac.id>
2025-06-25 22:04:44 +0700

    env port

--- config/database.js
+++ config/database.js
@@ -7,8 +7,9 @@ const db = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-     host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
   });
 
 export default db;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/router.js"
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -8,6 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+app.listen(process.env.DB_PORT, () => {
+  console.log("Server is running")
 })
